60c9e4c81980a60f56fefc6d3d0ddb175028a765
Hyeonjae Lee <odyflame@icloud.com>
2021-04-05 12:00:07 +0900

    Add New Project
    MemoApp, to refactoring CoreData, RxSwift, and MVVM

---
+++ SparkRefactoring/VocaSubsystem/VocaCoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  VocaCoreDataManager.swift
+//  VocaSubsystem
+//
+//  Created by Hanteo on 2021/04/05.
+//
+
+import Foundation
+
---
+++ SparkRefactoring/VocaSubsystem/VocaManager.swift
@@ -0,0 +1,8 @@
+//
+//  VocaManager.swift
+//  VocaSubsystem
+//
+//  Created by Hanteo on 2021/04/05.
+//
+
+import Foundation
